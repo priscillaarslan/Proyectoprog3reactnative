ddf03a2363f2ea28750963c3f8184df590d66080
martinadero <mdaireaux@udesa.edu.ar>
2023-06-06 14:41:06 -0300

    likes

--- proyectoProg3/src/components/Card.js
+++ proyectoProg3/src/components/Card.js
@@ -8,13 +8,28 @@ class Card extends Component {
     constructor(props) {
         super(props)
         this.state = {
-     
+             likeado:false,
+
             }
 
     }
     componentDidMount(){ 
       
     }
+    like(){
+        // el doc. entra al posteo individual, data.id esta antes de la data//
+        let posteo =  db.collection("posteos").doc(this.props.data.id)
+        //el if funciona con true o false, no hace falta igualarlo//
+       if (this.state.likeado) {
+        // update metodo de firebase para actualizar elementos de una coleccion de forma individual//
+        posteo.update({
+            
+        })
+       } else {
+        
+       }
+    }
+
 
 
 
@@ -22,13 +37,17 @@ class Card extends Component {
 
   
     render(){
-        console.log(this.props.data.data) 
+          
         return(
             <View>
                 <Text>Titulo del posteo: {this.props.data.data.Titulo}</Text>
                 <Text>Descripcion del posteo: {this.props.data.data.Descripcion}</Text>
                 <Text>Likes: {this.props.data.data.Likes.length}</Text>
-              
+                {this.state.likeado ? <TouchableOpacity onPress={() => this.like()}>
+                              <Text>Likear</Text>
+                     </TouchableOpacity> : <TouchableOpacity onPress={() => this.like()}>
+                                    <Text>Deslikear</Text>
+                </TouchableOpacity>} 
          </View>
        
           
