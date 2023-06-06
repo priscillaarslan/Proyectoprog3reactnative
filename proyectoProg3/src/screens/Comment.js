7a3e0e70e32d51e6a21ecab6c19f5b120ae18a65
Martina Mendez <martinamendez@Martinas-MacBook-Air-2.local>
2023-06-06 19:40:53 -0300

    creando comentarios

--- proyectoProg3/src/screens/Comment.js
+++ proyectoProg3/src/screens/Comment.js
@@ -9,11 +9,20 @@ class Comment extends Component {
     constructor() {
         super()
         this.state = {
+            escribiendoComentario:"",
+            comentarios: []
       
             }
 
     }
     componentDidMount(){ 
+        db.collection("posteos").doc(this.props.route.params.id).onSnapshot((docs) => {
+            this.setState({
+                comentarios:doc.data().comentarios
+
+            })
+        }
+
       
     }
 
@@ -25,7 +34,36 @@ class Comment extends Component {
        
         return(
             <View>
-                <Text>Soy un comentario</Text>
+                <Text>Pagina de comentarios</Text>
+                <Text>Los comentarios a este posteo son :</Text>
+                {
+                    this.state.comentarios.length==0? 
+                    <Text> Aun no hay comentarios </Text>: 
+                     <FlatList data={this.state.comentarios} keyExtractor={(data)=>data.id} renderItem={({item})=>  <Text>Este es un comentario :</Text> }
+                    >
+                    
+            </FlatList> 
+                }
+               
+
+            <TextInput 
+             placeholder='escribi un comentario' 
+             keyboardType='default'
+             onChangeText={texto=>this.setState({escribiendoComentario:texto})}
+             value= {this.state.escribiendoComentario}
+             />
+              
+            
+          
+            {
+                this.state.escribiendoComentario==""?
+               < TouchableOpacity>
+                 <Text> Escribir comentario </Text>
+               </TouchableOpacity>:
+                  <  TouchableOpacity onPress={()=>this.escribirComentario(this.state.escribiendoComentario)}> 
+                  <Text> Escribir comentario </Text>
+                </TouchableOpacity>
+               }
                
          </View>
        
