e1e65c9ddaa551c85a4903bc8f756b231277dd01
martinadero <mdaireaux@udesa.edu.ar>
2023-06-11 17:21:14 -0300

    cambiando iconos

--- proyectoProg3/src/components/Navegadorlogueado.js
+++ proyectoProg3/src/components/Navegadorlogueado.js
@@ -3,6 +3,8 @@ import Home from '../screens/Home';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs'
 import Addpost from '../screens/Addpost';
 import Profile from '../screens/Profile';
+import {FontAwesome} from '@expo/vector-icons';
+import { AntDesign } from '@expo/vector-icons';
 
 
 const Tab = createBottomTabNavigator();
@@ -10,9 +12,12 @@ class NavegadorLogueado extends Component {
     render(){
         return(
             <Tab.Navigator>
-            <Tab.Screen name='Home' component={Home} options={{ headerShown: false }} />
-            <Tab.Screen name='Addpost' component={Addpost} options={{ headerShown: false }} />
-            <Tab.Screen name='Profile' component={Profile} options={{ headerShown: false }} />
+            <Tab.Screen name='Home' component={Home} options={{ headerShown: false,
+             tabBarIcon: () => <AntDesign name='home' color='black' size={24} /> }} />
+            <Tab.Screen name='Addpost' component={Addpost} options={{ headerShown: false,
+           tabBarIcon: () => <AntDesign name="plus" color="black" size={24} />  }} />
+            <Tab.Screen name='Profile' component={Profile} options={{ headerShown: false,
+           tabBarIcon: () => <AntDesign name="profile" color="black" size={24} />  }} />
         </Tab.Navigator>
         )
     }
